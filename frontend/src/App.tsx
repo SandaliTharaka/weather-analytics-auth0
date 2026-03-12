4a87ca2268b1d76bd8e968a5674528b4ebca6085
SandaliTharaka <stharuka093@gmail.com>
2026-03-12 17:23:34 +0530

    Add Auth

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,8 +2,42 @@ import React from "react";
 import "./styles/globals.css";
 import Dashboard from "./pages/Dashboard";
 
+import { useAuth0 } from "@auth0/auth0-react";
+
 function App() {
-  return <Dashboard />;
+
+  const { loginWithRedirect, logout, isAuthenticated, user } = useAuth0();
+
+  if (!isAuthenticated) {
+    return (
+      <div style={{ textAlign: "center", marginTop: "100px" }}>
+        <h2>Weather Analytics Dashboard</h2>
+        <button onClick={() => loginWithRedirect()}>
+          Login
+        </button>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <p>Welcome {user?.name}</p>
+
+      <button
+        onClick={() =>
+          logout({ logoutParams: { returnTo: window.location.origin } })
+        }
+      >
+        Logout
+      </button>
+
+      return <Dashboard />;
+    </div>
+  );
 }
 
 export default App;
+
+
+
+
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,17 +2,24 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement,
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  document.getElementById("root") as HTMLElement
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+root.render(
+  <React.StrictMode>
+    <Auth0Provider
+      domain={process.env.REACT_APP_AUTH0_DOMAIN as string}
+      clientId={process.env.REACT_APP_AUTH0_CLIENT_ID as string}
+      authorizationParams={{
+        redirect_uri: window.location.origin
+      }}
+    >
+      <App />
+    </Auth0Provider>
+  </React.StrictMode>
+);
+
 reportWebVitals();
